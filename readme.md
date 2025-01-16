dba4a919016b4a7548d726c0e0a547e1d1c4df91
saitenasuk <saitenasuk@gmail.com>
2025-01-16 09:43:01 +0800

    更新版本号至0.0.22，修改readme内容，重构配置项，移除不必要的代码

--- readme.md
+++ readme.md
@@ -2,12 +2,8 @@
 
 [![npm](https://img.shields.io/npm/v/koishi-plugin-self-host-api?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-self-host-api)
 
-自行部署[kimi-free-api](https://github.com/LLM-Red-Team/kimi-free-api)服务
+自行部署类似服务[kimi-free-api](https://github.com/LLM-Red-Team/kimi-free-api)
 
-自行部署[Douyin_TikTok_Download_API](https://github.com/Evil0ctal/Douyin_TikTok_Download_API)服务
 
-抖音解析代码参考：[koishi-plugin-douyin](https://github.com/Evil0ctal/Douyin_TikTok_Download_API)
-
-开发中
 
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { Context, Schema, h } from "koishi";
 import {} from "koishi-plugin-markdown-to-image-service";
-import { title } from "process";
 
 export const name = "kimi-api";
 
@@ -13,10 +12,9 @@ export interface Config {
   tokens: string;
   url: string;
   use_search: any;
-  model: boolean;
+  model: string;
   multiRoundDialogue: boolean;
   use_markdownToImage: boolean;
-  analyze: any;
   tips: string;
 }
 
@@ -29,7 +27,7 @@ export const Config: Schema<Config> = Schema.object({
       "目前kimi限制普通账号每3小时内只能进行30轮长文本的问答(短文本不限)你可以通过提供多个账号的refresh_token并使用`,`拼接"
     )
     .required(),
-
+  model: Schema.string().description("模型名称").default("kimi"),
   // use_search: Schema.boolean().description("是否开启联网搜索").default(true),
   // model: Schema.boolean()
   use_search: Schema.intersect([
@@ -46,26 +44,6 @@ export const Config: Schema<Config> = Schema.object({
       Schema.object({}),
     ]),
   ]),
-  analyze: Schema.intersect([
-    Schema.object({
-      analyze: Schema.boolean()
-        .description("是否开启解析抖音，tiktok链接")
-        .default(false),
-    }),
-    Schema.union([
-      Schema.object({
-        analyze: Schema.const(true).required(),
-        link: Schema.string()
-          .description("Api地址示例：http://127.0.0.1:80")
-          .required(),
-      }),
-      Schema.object({}),
-    ]),
-  ]),
-  model: Schema.boolean()
-    .description("是否开启kimi+智能体切换功能")
-    .default(false)
-    .hidden(),
   multiRoundDialogue: Schema.boolean()
     .description("是否开启多轮对话")
     .default(false)
@@ -77,10 +55,10 @@ export const Config: Schema<Config> = Schema.object({
 });
 declare module "koishi" {
   interface Tables {
-    kimiData: kimiData;
+    self_host_api: SelfData;
   }
 }
-export interface kimiData {
+export interface SelfData {
   id: number;
   qid: string;
   conversation_id: string;
@@ -97,8 +75,7 @@ const text3: string =
 //解析失败文本
 const analyze_err_text: string = "可能的原因:\n\n视频已被删除或者链接不正确。";
 export async function apply(ctx: Context, config: Config) {
-  console.log("kimi-api apply");
-  ctx.model.extend("kimiData", {
+  ctx.model.extend("self_host_api", {
     // 各字段的类型声明
     id: "unsigned",
     qid: "char",
@@ -216,19 +193,6 @@ export async function apply(ctx: Context, config: Config) {
       throw error; // Re-throw the error to be handled by the caller
     }
   };
-
-  function formatMilliseconds(milliseconds) {
-    let seconds = Math.floor(milliseconds / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
-    seconds %= 60;
-    minutes %= 60;
-    const hh = String(hours).padStart(2, "0");
-    const mm = String(minutes).padStart(2, "0");
-    const ss = String(seconds).padStart(2, "0");
-    return `${hh}时${mm}分${ss}秒`;
-  }
-
   //判断是否markdown
   function isMarkdown(text: string): boolean {
     const markdownPatterns = [
@@ -321,97 +285,6 @@ export async function apply(ctx: Context, config: Config) {
         await _.session.bot.deleteMessage(_.session.channelId, tipMessageId);
       }
     });
-  ctx.on("message", async (session) => {
-    //不解析bot自己的消息
-    if (session.selfId === session.userId) return;
-    if (!config.analyze.analyze) return;
-    if (
-      session.content.includes("douyin.com") ||
-      session.content.includes("tiktok.com")
-    ) {
-      try {
-        const urlRegex = /(https?:\/\/[^\s]+)/g;
-        const url = session.content.match(urlRegex)[0];
-        console.log("url :>> ", url);
-        const response = await fetch(
-          `${config.analyze.link}/api/hybrid/video_data?url=${url}&minimal=false`
-        );
-        //解析到视频大小
-        if (!response.ok) {
-          return session.send(
-            <>
-              <quote id={session.messageId} />
-              解析失败! 该链接或许不支持
-            </>
-          );
-        }
-        const res = await response.json();
-        const { desc, author, statistics, video, duration } = res.data;
-        const { nickname } = author;
-        const { digg_count, share_count, comment_count, collect_count } =
-          statistics;
-        const { big_thumbs, bit_rate, cover } = video;
-        const video_info = bit_rate.filter(
-          (item: any) =>
-            item.format === "mp4" &&
-            (item.gear_name === "adapt_lowest_1080_1" ||
-              item.gear_name === "adapt_lowest_720_1") &&
-            item.is_h265 === 1
-        );
-        console.log("video_info :>> ", video_info);
-        const text5 =
-          "标题：" +
-          desc +
-          "\n作者：" +
-          nickname +
-          "\n点赞数：" +
-          digg_count +
-          "\t  分享数：" +
-          share_count +
-          "\n评论数：" +
-          comment_count +
-          "\t  收藏数：" +
-          collect_count;
-        console.log(text5);
-        await session.sendQueued(
-          <>
-            <quote id={session.messageId} />
-            {/* {h.image(imageBuffer, "image/png")} */}
-            <img
-              src={
-                big_thumbs[0]?.img_url
-                  ? big_thumbs[0]?.img_url
-                  : cover.url_list[0]
-              }
-            />
-            {text5}
-          </>
-        );
-        await session.sendQueued(
-          "视频发送中... 时长：" +
-            formatMilliseconds(duration) +
-            "  大小：" +
-            (video_info[0].play_addr.data_size / 1024 / 1024).toFixed(2) +
-            "MB"
-        );
-        await session.sendQueued(
-          <>
-            <video src={video_info[0].play_addr.url_list[0]} />
-          </>
-        );
-      } catch (error) {
-        console.log(error);
-        return session.send(
-          <>
-            <quote id={session.messageId} />
-            发生错误：{error.message}
-          </>
-        );
-      } finally {
-        await session.cancelQueued();
-      }
-    }
-  });
   // 派生式子指令
   // ctx
   //   .command("ai.test <message:text> 展示巨长的搜索结果")
